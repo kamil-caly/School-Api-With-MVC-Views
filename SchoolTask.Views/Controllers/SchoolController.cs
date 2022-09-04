8654dcd243357696d85e81954d22bf6b4f8f54c8
Kamil Cały <kamil.caly@vp.pl>
2022-09-04 13:44:14 +0200

    adding school views

--- SchoolTask.Views/Controllers/SchoolController.cs
+++ SchoolTask.Views/Controllers/SchoolController.cs
@@ -1,8 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SchoolTaskModels.Dtos;
 
 namespace SchoolTask.Views.Controllers
 {
     public class SchoolController : Controller
     {
+        private HttpClient httpClient;
+        public SchoolController()
+        {
+             httpClient = new HttpClient();
+        }
+        public async Task<IActionResult> Index()
+        {
+            using var result = await httpClient.GetAsync("https://localhost:44304/api/school");
+            var jsonSchoolsDtos = await result.Content.ReadAsStringAsync();
+
+            IEnumerable<SchoolDto> schoolDtos = JsonConvert.DeserializeObject<List<SchoolDto>>(jsonSchoolsDtos);
+
+            return View(schoolDtos);
+        }
     }
 }
