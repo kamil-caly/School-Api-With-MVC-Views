80f9980dc99656e38a9874cd66e81f1609dd1148
Kamil Cały <kamil.caly@vp.pl>
2022-09-04 18:09:28 +0200

    adding some fix into student index

--- SchoolTask.Views/Controllers/StudentController.cs
+++ SchoolTask.Views/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SchoolTaskModels.Dtos;
+using System.Net;
 
 namespace SchoolTask.Views.Controllers
 {
@@ -8,6 +9,7 @@ namespace SchoolTask.Views.Controllers
     {
         private HttpClient httpClient;
         private readonly string baseUrl = "https://localhost:44304/api/school";
+        private int SchoolId;
         public StudentController()
         {
             httpClient = new HttpClient();
@@ -16,18 +18,37 @@ namespace SchoolTask.Views.Controllers
         // GET
         public async Task<IActionResult> Index(int schoolId)
         {
+            this.SchoolId = schoolId;
             using var result = await httpClient.GetAsync(baseUrl + $"/{schoolId}/student");
             var jsonStudentDtos = await result.Content.ReadAsStringAsync();
-
-            if (jsonStudentDtos is null)
-            {
-                TempData["info"] = "Any student not found";
-                return RedirectToPage("School/Index");
-            }
 
             IEnumerable<StudentDto> studentDtos = JsonConvert.DeserializeObject<List<StudentDto>>(jsonStudentDtos);
 
             return View(studentDtos);
+        }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateStudentDto dto)
+        {
+            using var postResult = await httpClient.PostAsJsonAsync<CreateStudentDto>(baseUrl + $"/{SchoolId}/student", dto);
+
+            if (postResult.StatusCode == HttpStatusCode.Created)
+            {
+                TempData["info"] = "Student created successfully";
+            }
+            else
+            {
+                TempData["info"] = $"{postResult.StatusCode}";
+            }
+
+            return RedirectToAction("Index", SchoolId);
         }
 
     }
